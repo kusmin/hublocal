dea709ef1555102d5a86ae65abac056cfd43562b
Renan Ribeiro <renan.lagee@gmail.com>
2023-03-24 07:12:07 -0300

    configurando dotenv

--- backend/src/auth/constants.ts
+++ backend/src/auth/constants.ts
@@ -1,3 +1,3 @@
 export const jwtConstants = {
-  secret: 'mySecretKey',
+  secret: process.env.JWT_SECRET || 'mySecretKey',
 };
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  dotenv.config();
 
   const options = new DocumentBuilder()
     .setTitle('API hub')
